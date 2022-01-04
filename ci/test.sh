d98cd23b0404e094bf6e9f6113e14c64c79d30aa
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-01-04 11:15:46 -0800

    Migrate more util code to util and cleanup

--- ci/test.sh
+++ ci/test.sh
@@ -3,4 +3,8 @@
 set -e
 set -x
 
+go test -v github.com/koinos/koinos-util -coverprofile=./build/util.out -coverpkg=./...
+
+gcov2lcov -infile=./build/util.out -outfile=./build/util.info
+
 golint -set_exit_status ./...
---
+++ output.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/koinos/koinos-proto-golang/koinos"
+	"github.com/koinos/koinos-proto-golang/koinos/canonical"
+	"github.com/koinos/koinos-proto-golang/koinos/protocol"
+	"github.com/multiformats/go-multihash"
+
+	"google.golang.org/protobuf/proto"
+)
+
+// MultihashString returns a hex string representation of the given multihash
+func MultihashString(hash []byte) string {
+	return "0x" + hex.EncodeToString(hash)
+}
+
+// BlockString returns a string containing the given block's height and ID
+func BlockString(block *protocol.Block) string {
+	id := MultihashString(block.Id)
+	prevID := MultihashString(block.Header.Previous)
+	return fmt.Sprintf("Height: %d ID: %s Prev: %s", block.Header.Height, id, prevID)
+}
+
+// TransactionString returns a string containing the given transaction's ID
+func TransactionString(transaction *protocol.Transaction) string {
+	id := MultihashString(transaction.Id)
+	return fmt.Sprintf("ID: %s", string(id))
+}
+
+// BlockTopologyString returns a string representation of the BlockTopologyCmp
+func BlockTopologyString(topo *koinos.BlockTopology) string {
+	id := MultihashString(topo.Id)
+	prevID := MultihashString(topo.Previous)
+	return fmt.Sprintf("Height: %d ID: %s Prev: %s", topo.Height, id, prevID)
+}
+
+// HashMessage takes a protobuf message and returns the multihash of the message
+func HashMessage(message proto.Message) ([]byte, error) {
+	data, err := canonical.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+
+	hasher := sha256.New()
+	hasher.Write(data)
+
+	// Encode as multihash
+	mh, err := multihash.Encode(hasher.Sum(nil), multihash.SHA2_256)
+	if err != nil {
+		return nil, err
+	}
+
+	return mh, nil
+}
